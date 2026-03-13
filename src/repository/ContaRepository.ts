2955e5818ccac9cda117bf30c02b13f06ca16363
marlos11 <marlos-gama@hotmail.com>
2026-03-13 14:30:37 -0300

    chore:corrige tipo vatiavel numero destino

--- src/repository/ContaRepository.ts
+++ src/repository/ContaRepository.ts
@@ -11,5 +11,5 @@ export interface ContaRepository {
   //Métodos Bancarios
   sacar(numero: number, valor: number): void;
   depositar(numero: number, valor: number): void;
-  transferir(numeroOrigem: number, numeroDestino: NumberConstructor, valor: number): void;
+  transferir(numeroOrigem: number, numeroDestino: number, valor: number): void;
 }
