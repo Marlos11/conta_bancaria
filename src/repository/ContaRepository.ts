689d813a4d9d0bbbcb57c5f22fd70cdae535b601
marlos11 <marlos-gama@hotmail.com>
2026-03-12 22:33:29 -0300

    feature:cria interface com crud e metodos para listar,cadastrar,atualizar, deletar, sacar, depositar e transferir.

---
+++ src/repository/ContaRepository.ts
@@ -0,0 +1,15 @@
+import { Conta } from "../Model/Conta";
+
+export interface ContaRepository {
+  //CRUD da Conta
+  procurarPorNumero(numero: number): void;
+  listarTodos(): void;
+  cadastrar(conta: Conta): void;
+  atualizar(conta: Conta): void;
+  deletar(numero: number): void;
+
+  //Métodos Bancarios
+  sacar(numero: number, valor: number): void;
+  depositar(numero: number, valor: number): void;
+  transferir(numeroOrigem: number, numeroDestino: NumberConstructor, valor: number): void;
+}
